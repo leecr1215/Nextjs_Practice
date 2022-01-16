346fc175c682fad5ddd1d08165e08ec2a5ca8bd4
leecr1215 <leecr1215@naver.com>
2022-01-16 17:28:36 +0900

    finish #1.6

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Potato() {
   return (
     <div>
-      <NavBar />
-      <h1>Hello</h1>
+      <h1>About</h1>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <NavBar />
       <h1>Hello</h1>
     </div>
   );
