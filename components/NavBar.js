4ce917dd775304c537a07e80152917f0365b4cb6
leecr1215 <leecr1215@naver.com>
2022-01-17 21:21:09 +0900

    finish #2.2

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -12,36 +11,36 @@ export default function NavBar() {
           <a className={router.pathname === "/" ? "active" : ""}>Home</a>
         </Link>
         <Link href="/about">
-          <a className={router.pathname === "/about" ? "active" : ""}> About</a>
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
-        <style jsx>{`
-          nav {
-            display: flex;
-            gap: 10px;
-            flex-direction: column;
-            align-items: center;
-            padding-top: 20px;
-            padding-bottom: 10px;
-            box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-              rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-          }
-          img {
-            max-width: 100px;
-            margin-bottom: 5px;
-          }
-          nav a {
-            font-weight: 600;
-            font-size: 18px;
-          }
-          .active {
-            color: tomato;
-          }
-          nav div {
-            display: flex;
-            gap: 10px;
-          }
-        `}</style>
       </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
